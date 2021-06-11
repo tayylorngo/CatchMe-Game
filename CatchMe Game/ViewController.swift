c61318e539f0b62cde9f2d24272fc71d71f42fb6
Taylor Ngo <cs.taylorngo@gmail.com>
2021-06-11 03:08:26 -0400

    added target area

--- CatchMe Game/ViewController.swift
+++ CatchMe Game/ViewController.swift
@@ -3,6 +3,7 @@
 //  CatchMe Game
 //
 //  Created by Taylor Ngo on 6/10/21.
+//  112626118
 //
 
 import UIKit
@@ -10,13 +11,17 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var gameButton: UIButton!
+    @IBOutlet weak var targetArea: UIView!
+    
     static var score: Int = 0
     var newButtonX: CGFloat?
     var newButtonY: CGFloat?
+    var newAreaX: CGFloat?
+    var newAreaY: CGFloat?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        initButtonLoc()
+        initLoc()
         ViewController.score = 0
         updateScore()
     }
@@ -46,7 +51,7 @@ class ViewController: UIViewController {
         scoreLabel.text = "Score: " + String(ViewController.score);
     }
     
-    func initButtonLoc(){
+    func initLoc(){
         let halfWidth = gameButton.bounds.width / 2.0
         let halfHeight = gameButton.bounds.height / 2.0
         let minX = view.safeAreaInsets.left
@@ -63,6 +68,20 @@ class ViewController: UIViewController {
 
         gameButton.center.x = newButtonX!
         gameButton.center.y = newButtonY!
+        
+        let areaHalfWidth = targetArea.bounds.width / 2.0
+        let areaHalfHeight = targetArea.bounds.height / 2.0
+        let randomAreaX = CGFloat.random(in: minX...maxX - (areaHalfWidth * 2))
+        let randomAreaY = CGFloat.random(in: CGFloat(minY)...maxY - (areaHalfHeight * 2))
+        
+        newAreaX = randomAreaX + areaHalfWidth
+        newAreaY = randomAreaY + areaHalfHeight
+        targetArea.center.x = newAreaX!
+        targetArea.center.y = newAreaY!
+    }
+    
+    func checkIfInside(){
+        
     }
     
     override func viewDidLayoutSubviews() {
@@ -71,6 +90,12 @@ class ViewController: UIViewController {
         }
         if let buttonY = newButtonY{
             gameButton.center.y = buttonY
+        }
+        if let areaX = newAreaX{
+            targetArea.center.x = areaX
+        }
+        if let areaY = newAreaY{
+            targetArea.center.y = areaY
         }
     }
     
